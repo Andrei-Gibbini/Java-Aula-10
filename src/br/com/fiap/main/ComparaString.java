4471de1889b47386e3d15d37d7534637eda30d6b
Andrei-Gibbini <andreigibbini52@gmail.com>
2025-04-28 08:34:56 -0300

    Adicionados dois comentários explicando cada método usado.

--- src/br/com/fiap/main/ComparaString.java
+++ src/br/com/fiap/main/ComparaString.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 
 public class ComparaString {
     public static void main(String[] args) {
+        //o Método equals retorna um valor verdadeiro ou falso e deve sempre ter o  Try - Catch.
         String senha = JOptionPane.showInputDialog("digite sua senha");
         if (senha.equals("P4sswOrD")){
             JOptionPane.showMessageDialog(null, "Acesso concedido");
         } else {
             JOptionPane.showMessageDialog(null, "Acesso negado");
         }
+        //o Método equalIgnoreCase tem a mesma função do Equals normal, porém ele ignora se está em maiúscula ou não
         if (senha.equalsIgnoreCase("P4sswOrD")){
             JOptionPane.showMessageDialog(null, "Autoriado Acesso");
         } else {
